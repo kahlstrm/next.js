ef267b63debcd8e51a9ed8a61bb8d7e7460f3950
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-06-20 12:15:12 +0200

    reject actions with invalid RSC responses

---
+++ test/e2e/app-dir/actions/app/error-handling/error.js
@@ -0,0 +1,18 @@
+'use client' // Error components must be Client Components
+
+export default function Error({ error, reset }) {
+  console.log('@@@@@', error)
+  return (
+    <div>
+      <h2 id="error-text">{error}</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
